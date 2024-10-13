ce17a3033d937a17bb014f5f416025f6cad68d2d
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-10-14 01:24:01 +0530

    Time: 42 ms (67.62%) | Memory: 19.5 MB (76.12%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         int n = nums.size();
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-            greater<pair<int, pair<int, int>>>> pq;
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>,
+                greater<pair<int, pair<int, int>>>> pq;
 
         int mini = 1e9, maxi = -1e9;
-
         for (int i = 0; i < n; i++) {
             mini = min(mini, nums[i][0]);
             maxi = max(maxi, nums[i][0]);
@@ -20,8 +19,8 @@ public:
         while (!pq.empty()) {
             auto it = pq.top();
             pq.pop();
-            int miniVal = it.first;     
-            int row = it.second.first;
+            int miniVal = it.first;  
+            int row = it.second.first;  
             int col = it.second.second; 
             if (maxi - miniVal < diff) {
                 ansMin = miniVal;
@@ -39,5 +38,7 @@ public:
         }
 
         return {ansMin, ansMax};
-    }
-};
+    
+}
+}
+;
