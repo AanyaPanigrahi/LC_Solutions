2cae3058b8923ff5a8f35d96e896004ba62635f4
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-12-31 16:26:05 +0530

    Time: 3 ms (26.60%) | Memory: 12.9 MB (55.34%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int maxDay = days.back();
-        int minDay = days.front();
-        vector<int> dp(maxDay + 1, 0);
-        unordered_set<int> travelDays(days.begin(), days.end());
-
-        for (int i = minDay; i <= maxDay; i++) {
-            if (travelDays.find(i) == travelDays.end()) {
-                dp[i] = dp[i - 1];
-            } else {
-                dp[i] = dp[i - 1] + costs[0]; // 1-day pass
-                dp[i] = min(dp[i], (i - 7 >= 0 ? dp[i - 7] : 0) +
-                                       costs[1]); // 7-day pass
-                dp[i] = min(dp[i], (i - 30 >= 0 ? dp[i - 30] : 0) +
-                                       costs[2]); // 30-day pass
-            }
+        int M = days.back(), d0 = days[0];
+        vector<int> dp(M + 1, 0);
+        for (int d = d0, i = 0; d <= M; d++) {
+            if (d == days[i]) {
+                i++;
+                dp[d] = min({costs[0] + dp[max(d - 1, 0)],
+                             costs[1] + dp[max(d - 7, 0)],
+                             costs[2] + dp[max(d - 30, 0)]});
+            } else
+                dp[d] = dp[d - 1];
         }
-
-        return dp[maxDay];
+        return dp[M];
     }
 };
