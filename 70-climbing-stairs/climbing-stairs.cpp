c9b10c9c06c60cb320b67bc6f8b9da12cf7adfe1
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2025-08-12 14:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-
-    //memoization
-    int count(vector<int>& dp,int n){
+int ways(int n, vector<int>& dp) {
         if(n==0 || n==1) return 1;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=count(dp,n-1)+count(dp,n-2);
+        return dp[n]=ways(n-1,dp)+ways(n-2,dp);
     }
+
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        if(n<0) return -1;
-        return count(dp,n);
+        return ways(n,dp);
     }
 };
